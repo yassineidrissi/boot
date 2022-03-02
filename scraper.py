d0ff8adcd50c0dba5c27499142708df12fe864b0
yassine idrissi <yassine42idrissi@gmail.com>
2022-03-03 01:18:59 +0400

    2

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from http.server import executable
 from turtle import xcor
 import selenium
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,28 +20,43 @@ URL = 'https://candidature.1337.ma/piscines'
 
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome("/Users/yassine/Documents/boot/chromedriver", options=chrome_options)
+driver = webdriver.Chrome('/Users/yassine/Documents/boot/chromedriver', options=chrome_options)
 
-# headers = {
-# "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"
-# }
-# page = requests.get(URL, headers=headers)
+headers = {
+"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"
+}
+page = requests.get(URL, headers=headers)
+time.sleep(10)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# print(soup)
 user = "yassine1337idrissi@gmail.com"
 password = "Yassin@0661535096"
 
-username_login = soup.find("div",{"class:controls"})
-
+# username_login = driver.find_element_by_class_name("string email optional")
+# password_login = driver.find_element_by_class_name("password optional")
+# username_login.send_keys(user)
+# time.sleep(1)
+# password_login.send_keys(password)
+user = soup.find("input",{"class":"string email optional"})
+#how i will know that this code is work
+print(user)
 
 piscine = soup.find()
 price = soup.find(id="corePrice_desktop")
 
+button = driver.find_element_by_tag_name("Sign in")
+time.sleep(2)
+button.click()
+time.sleep(2)
+soup2 = BeautifulSoup(page.content,'html.parser')
+if soup2.text != soup.text:
+    print(soup2.text)
+    
 # print(piscine)
-print(username_login)
+# print(username_login)
 # print(title)
-# print(soup)
