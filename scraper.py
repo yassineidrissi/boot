95bef910dbbd4984c9974766c532f8a6cc41b4d2
yassine idrissi <yassine42idrissi@gmail.com>
2022-03-03 17:59:21 +0400

    test

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from http.server import executable
 from turtle import xcor
-import selenium
 import time
+import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -29,16 +29,17 @@ headers = {
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"
 }
 page = requests.get(URL, headers=headers)
-time.sleep(10)
+time.sleep(5)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
 
 # print(soup)
 user = "yassine1337idrissi@gmail.com"
 password = "Yassin@0661535096"
 
-# username_login = driver.find_element_by_class_name("string email optional")
-# password_login = driver.find_element_by_class_name("password optional")
+username_login = driver.find_element_by_class_name("string email optional")
+password_login = driver.find_element_by_class_name("password optional")
 # username_login.send_keys(user)
 # time.sleep(1)
 # password_login.send_keys(password)
@@ -49,14 +50,14 @@ print(user)
 piscine = soup.find()
 price = soup.find(id="corePrice_desktop")
 
-button = driver.find_element_by_tag_name("Sign in")
+# button = driver.find_element_by_tag_name("Sign in")
 time.sleep(2)
 button.click()
 time.sleep(2)
 soup2 = BeautifulSoup(page.content,'html.parser')
 if soup2.text != soup.text:
     print(soup2.text)
-    
+
 # print(piscine)
 # print(username_login)
 # print(title)
