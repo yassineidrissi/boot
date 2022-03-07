d23f5b41731176b0664ba31bed8560484f71e0cc
yassine idrissi <yassine42idrissi@gmail.com>
2022-03-07 04:23:19 +0400

    whit group msj

--- scraper.py
+++ scraper.py
@@ -14,6 +14,14 @@ import time
 URL = 'https://candidature.1337.ma/piscines'
 
 
+def telegram_bot_sendtext(bot_message):
+    bot_token = "5133253099:AAG-g5TgU-zZtKP8HEUaLqWXkMRcz8Slm6I"
+    bot_chatID = '-783359041'
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
 chrome_options = Options()
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -49,11 +57,13 @@ soup2 = BeautifulSoup(page2.content, "html.parser")
 if soup != soup2:
     print("I m insade the web site")
 while True:
-    soup3 = BeautifulSoup(page2.content, "html.parser")
+    # soup3 = BeautifulSoup(page2.content, "html.parser")
     if soup3 != soup2:
         print("there is a new piscine")
+        telegram_bot_sendtext("💩makayn db ta piscine😴")
     else:
         print("there is no piscine")
+        telegram_bot_sendtext("💩makayn db ta piscine😴")
     time.sleep(10)
 
 # driver.close()
